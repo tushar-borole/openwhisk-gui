e8dfee893dd465580f15a41a1e5b17cff91e61d5
Tushar  Borole <tushariscoolster@gmail.com>
2019-01-06 18:25:49 -0800

    open whisk files added

--- app/server.js
+++ app/server.js
@@ -1,16 +1,28 @@
 const express = require('express')
+const openwhisk = require('openwhisk')
 
 const app = express()
 
 app.get('/', (req, res) => {
-  const openwhisk = require('openwhisk')
   const options = {
-    apihost: 'openwhisk.ng.bluemix.net',
-    api_key: '...'
+    api: 'https://10.0.0.243/api/v1/',
+    api_key: '23bc46b1-71f6-4ed5-8c54-816aa4f8c502:123zO3xZCLrMN6v2BKK1dXYFpXlPkccOFqm12CdAsMgRU4VrNZ9lyGVCGuMDGIwP',
+    namespace: 'guest',
+    ignore_certs: true
   }
-  const ow = openwhisk(options)
-  ow.actions.invoke('sample')
-    .then(result => console.log(result))
+
+  const wsk = openwhisk(options)
+
+  wsk.namespaces.list()
+    .then((packages) => {
+      console.log('PACKAGES')
+      console.log(packages)
+      return { packages }
+    })
+    .catch((err) => {
+      console.log(err)
+      return err
+    })
 })
 
 app.listen(3001)
